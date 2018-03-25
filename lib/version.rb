9b144f9a578fa5ff5ecb71ace75e98d9a986ca26
Toby Clemson <tobyclemson@gmail.com>
2018-03-25 21:59:36 +0100

    Fix release task.

--- lib/version.rb
+++ lib/version.rb
@@ -1,19 +1,21 @@
 module Semantic
   class Version
     def rc!
-      if @pre.nil?
-        increment!(:minor)
-        @pre = 'rc.1'
-        return
+      new_version = clone
+
+      if new_version.pre.nil?
+        new_version.increment!(:minor)
+        new_version.pre = 'rc.1'
+        return new_version
       end
 
-      if @pre =~ /^rc\.\d+$/
-        @pre = "rc.#{Integer(@pre.delete('rc.')) + 1}"
-        return
+      if new_version.pre =~ /^rc\.\d+$/
+        new_version.pre = "rc.#{Integer(new_version.pre.delete('rc.')) + 1}"
+        return new_version
       end
 
       raise RuntimeError.new(
-          "Error: pre segment '#{@pre}' is does not look like 'rc.n'")
+          "Error: pre segment '#{new_version.pre}' is does not look like 'rc.n'")
     end
   end
 end
