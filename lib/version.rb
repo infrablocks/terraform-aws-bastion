354e830940fb02beb0620f6c62f8bbf09db56cb9
Toby Clemson <tobyclemson@gmail.com>
2018-03-25 20:46:31 +0100

    Switch to release candidate tags for automated releases.

---
+++ lib/version.rb
@@ -0,0 +1,18 @@
+module Semantic
+  class Version
+    def rc!
+      if @pre.nil?
+        @pre = 'rc.1'
+        return
+      end
+
+      if @pre =~ /^rc\.\d+$/
+        @pre = "rc.#{Integer(@pre.delete('rc.')) + 1}"
+        return
+      end
+
+      raise RuntimeError.new(
+          "Error: pre segment '#{@pre}' is does not look like 'rc.n'")
+    end
+  end
+end
